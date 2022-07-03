19db3de8a0da370909ac384d8ff127a70e998dc6
Gabriel Stabile Martins <gabrielstabilemartins@gmail.com>
2022-07-03 20:30:39 -0300

    adiciona info de peso ideal

--- mod3sem1/imc.py
+++ mod3sem1/imc.py
@@ -3,4 +3,9 @@ altura = float(input('Digite sua altura: '))
 peso = float(input('Digite seu peso: '))
 
 imc = peso / (altura*altura)
+limite = 25
+if limite >= imc:
+    print("Acima do peso ideal")
+else:
+    print("está no peso ideal")
 print(imc)
